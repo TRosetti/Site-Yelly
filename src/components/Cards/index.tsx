0ed4522e96ecf477e6fccd37fc5d65e530e8b6b1
TRosetti <105075815+TRosetti@users.noreply.github.com>
2025-07-11 15:45:53 -0300

    update cards

--- src/components/Cards/index.tsx
+++ src/components/Cards/index.tsx
@@ -5,7 +5,7 @@ export function Cards(){
         <section>
             <div className={styles.cardContainer}>
                 <div className={styles.linha1}>
-                    <div className={`${styles.card} ${styles.card1}`} style={{background: "url('/Cards/taxas-exclusivas.png')"}}>
+                    <div className={`${styles.card} ${styles.card1}`} >
                         <h3>Taxas Exclusivas para Todos</h3>
                         <h2>As melhores taxas para você CPF ou CNPJ</h2>
                         <a href="#">
@@ -16,7 +16,7 @@ export function Cards(){
                 </div>
 
                 <div className={styles.linha2}>
-                    <div className={`${styles.card} ${styles.card2}`} style={{background: "url('/Cards/recebimento-rapido.png')"}}>
+                    <div className={`${styles.card} ${styles.card2}`} >
                     <h3>Recebimento rápido</h3>
                     <h2>Receba seu dinheiro em até um dia</h2>
                     <a href="#">
@@ -24,9 +24,9 @@ export function Cards(){
                         <img src="/setaAmarela2.svg" alt="Seta" />
                     </a>
                 </div>
-               <div className={`${styles.card} ${styles.card3}`} style={{background: "url('/Cards/alta-performace.png')"}}>
+               <div className={`${styles.card} ${styles.card3}`} >
                     <h3>Alta performace</h3>
-                    <h2>A máquina mais moderna do mercado!</h2>
+                    <h2>A máquina mais <br /> moderna do mercado!</h2>
                     <a href="#">
                         Conheça a Yelly Smart
                         <img src="/setaAmarela2.svg" alt="Seta" />
@@ -34,7 +34,7 @@ export function Cards(){
                 </div>
                 </div>
                 <div className={styles.linha3}>
-                    <div className={`${styles.card} ${styles.card4}`} style={{background: "url('/Cards/SeloRA1000.png')"}}>
+                    <div className={`${styles.card} ${styles.card4}`} >
                         <h3>selo RA1000</h3>
                         <h2>Excelência no atendimento</h2>
                         <a href="#">
@@ -42,7 +42,7 @@ export function Cards(){
                             <img src="/setaAmarela2.svg" alt="Seta" />
                         </a>
                     </div>
-                    <div className={`${styles.card} ${styles.card5}`} style={{background: "url('/Cards/redes-sociais.png')"}}>
+                    <div className={`${styles.card} ${styles.card5}`} >
                         <h3>redes sociais</h3>
                         <h2>Siga a Yelly no Instagram e fique por dentro das novidades!</h2>
                         <a href="#">
@@ -50,7 +50,7 @@ export function Cards(){
                             <img src="/setaAmarela2.svg" alt="Seta" />
                         </a>
                     </div>
-                    <div className={`${styles.card} ${styles.card6}`} style={{background: "url('/Cards/link-de-pagamento.png')"}}>
+                    <div className={`${styles.card} ${styles.card6}`} >
                         <h3>link de pagamento</h3>
                         <h2>Novidade na Yelly!</h2>
                         <a href="#">
